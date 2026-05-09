e72adb7678eed8cbb90365d00cf423eaa8c2be42
MostafaMahmoudtv <mm4582@fayoum.edu.eg>
2026-05-09 15:40:00 +0300

    new

--- js/payment.js
+++ js/payment.js
@@ -4,3 +4,17 @@ const nav = document.querySelector("nav");
 menuToggle.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+  const currentPage = window.location.pathname.split("/").pop();
+
+  const navLinks = document.querySelectorAll("nav ul li a");
+
+  navLinks.forEach((link) => {
+    const linkPage = link.getAttribute("href").split("/").pop();
+
+    if (
+      linkPage === currentPage ||
+      (currentPage === "" && link.getAttribute("href") === "/")
+    ) {
+      link.classList.add("active");
+    }
+  });
